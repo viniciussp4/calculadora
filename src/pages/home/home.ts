d38daad88699bc28882b10e0e9dfb15a9a286ee1
viniciussp4 <vinicius-sp@hotmail.com>
2018-12-04 20:01:32 -0200

    inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  valores = {
+    num1: '0',
+    num2: '0',
+    resultado: 0
+  }
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  soma(){
+    this.valores.resultado = parseInt(this.valores.num1) + parseInt(this.valores.num2);
   }
 
 }
